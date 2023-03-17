10a62c12b8450d8e18f208d89efb29fa836dcbc8
crowdbotics-dev <engineering@crowdbotics.com>
2023-03-17 14:54:01 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+class Article(models.Model):
+    'Generated Model'
+    author = models.CharField(max_length=256,)
+    image = models.URLField()
+    title = models.CharField(max_length=256,)
+    body = models.CharField(max_length=256,)
