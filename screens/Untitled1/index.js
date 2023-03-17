1735e0b622ab4777257ab6c8c6d92d6587244bba
Daniel Sousa <daniel.s@crowdbotics.com>
2023-03-17 14:29:56 +0000

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,17 +1,27 @@
+import { updateItem } from "../../modules/faq/store/index.js";
+import { useDispatch } from "react-redux";
 import { Pressable } from "react-native";
 import { Text } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
+  const dispatch = useDispatch();
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><Pressable><Pressable><Text style={styles.EDOFptUa}> </Text></Pressable></Pressable></ScrollView>
+    }}><Pressable><Pressable onPress={onSubmit}><Text style={styles.EDOFptUa}> </Text></Pressable></Pressable></ScrollView>
     </SafeAreaView>;
+
+  const onSubmit = () => {
+    dispatch(updateItem({
+      lorem,
+      email: localEmail
+    }));
+  };
 };
 
 const styles = StyleSheet.create({
