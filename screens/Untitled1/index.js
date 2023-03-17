a753aea8d0d5493000cda72f156a135efc7cd325
Daniel <daniel.s@crowdbotics.com>
2023-03-17 14:49:06 +0000

    scaffold hotfixes

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,32 +1,35 @@
-import { updateItem } from "../../modules/faq/store/index.js";
-import { useDispatch } from "react-redux";
-import { Pressable } from "react-native";
-import { Text } from "react-native";
-import React from "react";
-import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+//import { updateItem } from "../../modules/faq/store/index.js";
+import { useDispatch } from "react-redux"
+import { Pressable } from "react-native"
+import { Text } from "react-native"
+import React from "react"
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native"
 
 const Untitled1 = () => {
-  const dispatch = useDispatch();
-  return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      backgroundColor: '#f0f0f1',
-      padding: 10,
-      position: 'relative',
-      flex: 1
-    }}><Pressable><Pressable onPress={onSubmit}><Text style={styles.EDOFptUa}> </Text></Pressable></Pressable></ScrollView>
-    </SafeAreaView>;
-
-  const onSubmit = () => {
-    dispatch(updateItem({
-      lorem,
-      email: localEmail
-    }));
-  };
-};
+  const dispatch = useDispatch()
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView
+        contentContainerStyle={{
+          backgroundColor: "#f0f0f1",
+          padding: 10,
+          position: "relative",
+          flex: 1
+        }}
+      >
+        <Pressable>
+          <Pressable>
+            <Text style={styles.EDOFptUa}> </Text>
+          </Pressable>
+        </Pressable>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
   },
   EDOFptUa: {
     width: 100,
@@ -35,5 +38,5 @@ const styles = StyleSheet.create({
     fontSize: 14,
     borderRadius: 0
   }
-});
-export default Untitled1;
+})
+export default Untitled1
